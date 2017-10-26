b09cf5c953a46892a7e4e3bf9f00df61d5cfb028
xirts <erinversfeld@gmail.com>
2017-10-26 08:41:57 +0200

    [dominate] remove legacy code

--- divide_and_conquer/dominate.py
+++ divide_and_conquer/dominate.py
@@ -1,17 +1,3 @@
-from functools import cmp_to_key
-
-
-def coord_comparison(p1, p2):
-    y_diff = p1[1] - p2[1]
-    if y_diff != 0:
-        return y_diff
-    return p1[0] - p2[0]
-
-
-def comparator(threshold):
-    return coord_comparison(p1, p2)
-
-
 def dominating_pairs(ordered_coordinates):
     if len(ordered_coordinates) == 2 or len(ordered_coordinates) == 1:
         return 0 if len(ordered_coordinates) == 1 or ordered_coordinates[0][1] > ordered_coordinates[1][1] else 1
@@ -30,15 +16,6 @@ def dominating_pairs(ordered_coordinates):
 
     return count + dominating_pairs(ordered_coordinates[:half_len]) + dominating_pairs(ordered_coordinates[half_len:])
 
-
-def get_pairs(strng):
-    arr = [int(i) for i in strng.strip().split(' ')]
-    pairs = []
-    for i in range(0, len(arr), 2):
-        coord_pair = [arr[i], arr[i+1]]
-        pairs.append(coord_pair)
-
-    return pairs
 
 if __name__ == "__main__":
     points = [int(i) for i in input().strip().split()]
